372e85a703580665205f7d0ec78668d9c9bbdc2f
Hussain Ali <iamhussainali2@gmail.com>
2024-05-03 09:08:02 +0530

    set the rotes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Error from "./pages/Error/Error";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Root from "./components/Root/Root";
+import Home from "./pages/Home/Home";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,13 @@
+const Error = () => {
+  return (
+    <div className="">
+      <h1>Not Found</h1>
+      <a href="/">
+        {" "}
+        <button>Home</button>
+      </a>
+    </div>
+  );
+};
+
+export default Error;
